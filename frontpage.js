2c5b34cdb01910cc866a550601d51b88c2f5d8af
amandatolstruplaursen <amanda.t.laursen@hotmail.com>
2022-09-25 22:18:06 +0200

    Test

--- frontpage.js
+++ frontpage.js
@@ -36,72 +36,71 @@ if (minutes.length === 1){
 }
 
  */
-window.onload = function() {
 
 
-    var firstPlace;
-    var secondPlace;
-    var thirdPlace;
-    var fourthPlace;
+var firstPlace;
+var secondPlace;
+var thirdPlace;
+var fourthPlace;
 
-    var today = new Date();
-    var hours = today.getHours()
-    if (hours.length === 1) {
-        numberToPicture(0, 1)
-        secondPlace = hours;
-        numberToPicture(secondPlace, 2)
+var today = new Date();
+var hours = today.getHours()
+if (hours.length === 1){
+    numberToPicture(0, 1)
+    secondPlace = hours;
+    numberToPicture(secondPlace, 2)
 
-    } else {
-        firstPlace = hours[0]
-        secondPlace = hours[1]
-        numberToPicture(firstPlace, 1)
-        numberToPicture(secondPlace, 2)
-    }
+} else {
+    firstPlace = hours[0]
+    secondPlace = hours[1]
+    numberToPicture(firstPlace, 1)
+    numberToPicture(secondPlace, 2)
+}
 
-    var minutes = today.getMinutes()
-    if (minutes.length === 1) {
-        numberToPicture(0, 3)
-        fourthPlace = minutes;
-        numberToPicture(fourthPlace, 4)
-    } else {
-        thirdPlace = minutes[0]
-        fourthPlace = minutes[1]
-        numberToPicture(thirdPlace, 3)
-        numberToPicture(fourthPlace, 4)
-    }
+var minutes = today.getMinutes()
+if (minutes.length === 1){
+    numberToPicture(0, 3)
+    fourthPlace = minutes;
+    numberToPicture(fourthPlace, 4)
+} else {
+    thirdPlace = minutes[0]
+    fourthPlace = minutes[1]
+    numberToPicture(thirdPlace, 3)
+    numberToPicture(fourthPlace, 4)
+}
 
-    function numberToPicture(number, id) {
-        switch (number) {
-            case 0:
-                document.getElementById(id).src = 'images/Zero.png';
-                break;
-            case 1:
-                document.getElementById(id).src = 'images/One.png';
-                break;
-            case 2:
-                document.getElementById(id).src = 'images/Two.jpg';
-                break;
-            case 3:
-                document.getElementById(id).src = 'images/Three.png';
-                break;
-            case 4:
-                document.getElementById(id).src = 'images/Four.png';
-                break;
-            case 5:
-                document.getElementById(id).src = 'images/Five.jpg';
-                break;
-            case 6:
-                document.getElementById(id).src = 'images/Six.jpg';
-            case 7:
-                document.getElementById(id).src = 'images/Seven2.jpg';
-                break;
-            case 8:
-                document.getElementById(id).src = 'images/Eight.png';
-                break;
-            case 9:
-                document.getElementById(id).src = 'images/Nine2.png';
-                break;
-        }
+function numberToPicture(number, id) {
+    switch (number){
+        case 0:
+            document.getElementById(id).src='images/Zero.png';
+            break;
+        case 1:
+            document.getElementById(id).src='images/One.png';
+            break;
+        case 2:
+            document.getElementById(id).src='images/Two.jpg';
+            break;
+        case 3:
+            document.getElementById(id).src='images/Three.png';
+            break;
+        case 4:
+            document.getElementById(id).src='images/Four.png';
+            break;
+        case 5:
+            document.getElementById(id).src='images/Five.jpg';
+            break;
+        case 6:
+            document.getElementById(id).src='images/Six.jpg';
+            break;
+        case 7:
+            document.getElementById(id).src='images/Seven2.jpg';
+            break;
+        case 8:
+            document.getElementById(id).src='images/Eight.png';
+            break;
+        case 9:
+            document.getElementById(id).src='images/Nine2.png';
+            break;
     }
 }
 
