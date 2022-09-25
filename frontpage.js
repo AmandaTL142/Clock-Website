11f265013905943aba566d9ce52f15724b3a35e7
amandatolstruplaursen <amanda.t.laursen@hotmail.com>
2022-09-25 22:51:02 +0200

    To former for tal

--- frontpage.js
+++ frontpage.js
@@ -37,70 +37,118 @@ if (minutes.length === 1){
 
  */
 
-
 var firstPlace;
 var secondPlace;
 var thirdPlace;
 var fourthPlace;
+var randomNumber;
 
 var today = new Date();
 
 var hours = today.getHours()
 if (hours.length === 1){
-    numberToPicture(0, 1)
+    randomNumber = Math.floor(Math.random() * 2)
+    numberToPicture(0, 1, randomNumber)
     secondPlace = hours;
-    numberToPicture(Number(secondPlace), 2)
+    randomNumber = Math.floor(Math.random() * 2)
+    numberToPicture(Number(secondPlace), 2, randomNumber)
 
 } else {
     firstPlace = String(hours)[0]
     secondPlace = String(hours)[1]
-    numberToPicture(Number(firstPlace), 1)
-    numberToPicture(Number(secondPlace), 2)
+    randomNumber = Math.floor(Math.random() * 2)
+    numberToPicture(Number(firstPlace), 1, randomNumber)
+    randomNumber = Math.floor(Math.random() * 2)
+    numberToPicture(Number(secondPlace), 2, randomNumber)
 }
 
 var minutes = today.getMinutes()
 if (minutes.length === 1){
-    numberToPicture(0, 3)
+    randomNumber = Math.floor(Math.random() * 2)
+    numberToPicture(0, 3, randomNumber)
     fourthPlace = minutes;
-    numberToPicture(Number(fourthPlace), 4)
+    randomNumber = Math.floor(Math.random() * 2)
+    numberToPicture(Number(fourthPlace), 4, randomNumber)
 } else {
     thirdPlace = String(minutes)[0]
     fourthPlace = String(minutes)[1]
-    numberToPicture(Number(thirdPlace), 3)
-    numberToPicture(Number(fourthPlace), 4)
+    randomNumber = Math.floor(Math.random() * 2)
+    numberToPicture(Number(thirdPlace), 3, randomNumber)
+    randomNumber = Math.floor(Math.random() * 2)
+    numberToPicture(Number(fourthPlace), 4, randomNumber)
 }
 
-function numberToPicture(number, id) {
+function numberToPicture(number, id, randomNumber) {
     switch (number){
         case 0:
-            document.getElementById(id).src='images/Zero.png';
+            if (randomNumber===0){
+                document.getElementById(id).src='images/Zero.png';
+            } else{
+                document.getElementById(id).src='images/Zero2.webp';
+            }
             break;
         case 1:
-            document.getElementById(id).src='images/One.png';
+            if (randomNumber===0) {
+                document.getElementById(id).src = 'images/One.png';
+            }else{
+                document.getElementById(id).src = 'images/One2.png';
+            }
             break;
         case 2:
-            document.getElementById(id).src='../images/Two.jpg';
+            if (randomNumber===0) {
+                    document.getElementById(id).src = 'images/Two.jpg';
+                }else{
+                    document.getElementById(id).src = 'images/Two2.jpg';
+                }
             break;
         case 3:
-            document.getElementById(id).src='images/Three.png';
+            if (randomNumber===0){
+                document.getElementById(id).src='images/Three.png';
+            }else{
+                document.getElementById(id).src='images/Three2.jpg';
+            }
             break;
         case 4:
-            document.getElementById(id).src='images/Four.png';
+            if (randomNumber===0){
+                document.getElementById(id).src='images/Four.png';
+            }else{
+                ocument.getElementById(id).src='images/Four2.jpg';
+            }
             break;
         case 5:
-            document.getElementById(id).src='images/Five.jpg';
+            if (randomNumber===0){
+                document.getElementById(id).src='images/Five.jpg';
+            }else{
+                document.getElementById(id).src='images/Five2.webp';
+            }
             break;
         case 6:
-            document.getElementById(id).src='images/Six.jpg';
+            if (randomNumber===0){
+                document.getElementById(id).src='images/Six.jpg';
+            }else{
+                document.getElementById(id).src='images/Six2.png';
+            }
             break;
         case 7:
-            document.getElementById(id).src='images/Seven2.jpg';
+            if (randomNumber===0){
+                document.getElementById(id).src='images/Seven.webp';
+            }else{
+                document.getElementById(id).src='images/Seven2.jpg';
+            }
             break;
         case 8:
-            document.getElementById(id).src='images/Eight.png';
+            if (randomNumber===0){
+                document.getElementById(id).src='images/Eight.png';
+            }else{
+                document.getElementById(id).src='images/Eight2.jpg';
+            }
             break;
         case 9:
-            document.getElementById(id).src='images/Nine2.png';
+            if (randomNumber===0){
+                document.getElementById(id).src='images/Nine.webp';
+            }else{
+                document.getElementById(id).src='images/Nine2.png';
+            }
             break;
     }
 }
